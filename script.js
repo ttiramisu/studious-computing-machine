186981d59aa21dc4435817272ec164428f439708
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-01-12 16:40:04 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -51,24 +51,29 @@ document.addEventListener('contextmenu', event => event.preventDefault());
 
 
 document.onkeydown = function (e) {
- 
+
   // disable F12 key
-  if(e.keyCode == 123) {
-      return false;
+  if (e.keyCode == 123) {
+    return false;
   }
 
   // disable I key
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
-      return false;
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 67) {
+    return false;
+  }
+
+  // disable I key
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+    return false;
   }
 
   // disable J key
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-      return false;
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+    return false;
   }
 
   // disable U key
-  if(e.ctrlKey && e.keyCode == 85) {
-      return false;
+  if (e.ctrlKey && e.keyCode == 85) {
+    return false;
   }
 }
